49ecf4fec5dc1eb63a7df6906171aa386bd7566d
YairSinger <yairsinger52@gmail.com>
2025-03-17 12:57:47 +0200

    moving self projects to separated repository

--- prjects/bank-app/src/main/java/com/bank/exception/TokenRefreshException.java
+++
@@ -1,9 +0,0 @@
-package com.bank.exception;
-
-public class TokenRefreshException extends Throwable {
-
-    public TokenRefreshException(String token, String string) {
-        super(string + " " + token);        
-    }
-
-}
